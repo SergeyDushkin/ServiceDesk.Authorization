7068006835425924dcf2049df9818459beca0591
SergeyDushkin <leirbythe@gmail.com>
2017-03-24 16:57:09 +0300

    init

---
+++ Config.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServerWithAspNetIdentity
+{
+    public class Config
+    {
+        // scopes define the resources in your system
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API"),
+                new ApiResource("helpdesk", "helpdesk api") {
+
+                    Description = "helpdesk api features and data",
+                    UserClaims = new [] { "name", "given_name", "family_name", "email", "role", "feature", "website" }
+                }
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris = { "http://localhost:4200/callback.html", "http://52.178.193.205/login" },
+                    PostLogoutRedirectUris = { "http://localhost:4200/index.html", "http://52.178.193.205/login" },
+                    AllowedCorsOrigins = { "http://localhost:4200", "http://52.178.193.205" },
+
+                    AllowOfflineAccess = true,
+
+                    AllowedScopes =
+                        {
+                            IdentityServerConstants.StandardScopes.OpenId,
+                            IdentityServerConstants.StandardScopes.Profile,
+                            "offline_access",
+                            "helpdesk"
+                        },
+                }
+            };
+        }
+    }
+}
